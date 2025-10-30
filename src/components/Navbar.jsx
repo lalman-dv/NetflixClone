cab37831853d4fee6fd915df0d782929a307a008
Lalman <lalmanchaudhary2@gmail.com>
2025-10-30 16:26:23 +0530

    making navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,19 +5,21 @@ import profile_img from "/public/assets/profile_img.png";
 import caret_icon from "/public/assets/caret_icon.svg";
 export const Navbar = () => {
   return (
-    <div className="w-full p- ">
-      <div>
-        <img src={logo} alt="logo" />
-        <ul>
-          <li>Home</li>
-          <li>TV Shows</li>
-          <li>Movies</li>
-          <li>New & Popular</li>
-          <li>My List</li>
-          <li>Browse By Languages</li>
-        </ul>
+    <div className="w-full px-5 py-1.5 flex justify-between fixed  text-gray-200 z-10">
+      <div className="flex items-center gap-12">
+        <img className="w-22" src={logo} alt="logo" />
+        
+          <ul className="flex list-none gap-5 cursor-pointer" >
+            <li>Home</li>
+            <li>TV Shows</li>
+            <li>Movies</li>
+            <li>New & Popular</li>
+            <li>My List</li>
+            <li>Browse By Languages</li>
+          </ul>
+        
       </div>
-      <div>
+      <div className="flex gap-5 items-center">
         <img src={search_icon} alt="search_icon_logo" />
         <p>Children</p>
         <img src={bell_icon} alt="bell_icon_logo" />
